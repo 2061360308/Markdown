e9c8f43ca91f195ab1b8f1f20780631b64ca44d7
2061360308 <2061360308@qq.com>
2025-01-09 01:35:17 +0800

    fix:netlify构建失败

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -2,8 +2,11 @@ exports.handler = async function (event, context) {
   const client_id = process.env.GITHUB_CLIENT_ID;
   const client_secret = process.env.GITHUB_CLIENT_SECRET;
 
+  // 获取查询参数
+  const queryParams = event.queryStringParameters;
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: queryStringParameters }),
+    body: JSON.stringify({ message: queryParams }),
   };
 };
